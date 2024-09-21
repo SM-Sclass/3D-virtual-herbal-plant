7d4d9575d06f4268eaab8511392b14a230daaf0e
SM-Sclass <smclover376@gmail.com>
2024-09-21 20:43:51 +0530

    image added

--- src/app/(pages)/Tour/medicinal/page.jsx
+++ src/app/(pages)/Tour/medicinal/page.jsx
@@ -3,7 +3,6 @@ import medicinal from '../../../utils/medicinal'
 import PlantCard from '../../../component/Plantcard'
 
 function page() {
-  console.log(medicinal[1])
   return (
     <div className="bg-white h-full rounded-md flex flex-col items-center">
       <div className="container my-6 py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
