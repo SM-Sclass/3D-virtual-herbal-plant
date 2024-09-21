304ddb95530e59936531d0d1c32cc1d2a3a1b2da
meleo2125 <mukeshprajapat3093@gmail.com>
2024-09-21 20:25:19 +0530

    plant card

--- src/app/component/Plantcard.jsx
+++ src/app/component/Plantcard.jsx
@@ -28,27 +28,30 @@ function PlantCard({ plant }) {
   const imagePath = `/assets/img/explore/${lowertitle}.png`;
 
   return (
-    <div className='relative'>
-      <Link href={`/plants/${lowertitle}`} className='cursor-pointer'>
-        {/* Apply hover effect on the entire card */}
+    <div className="relative">
+      <Link href={`/plants/${lowertitle}`} className="cursor-pointer">
+        {/* Hover effect applied to the entire card */}
         <div className="bg-green-500 hover:bg-green-700 hover:shadow-2xl hover:-translate-y-1 duration-200 rounded-2xl relative w-full flex flex-col items-center justify-center">
           {/* Image container */}
           <div className="relative w-full h-64">
-            <Image 
-              src={imagePath} 
-              alt={title} 
-              layout="fill" // Use layout fill to cover the parent div
-              objectFit="cover" // Adjust the image to cover the container
-              className="rounded-t-2xl" // Rounded top corners of the image
+            <Image
+              src={imagePath}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-t-2xl"
             />
           </div>
-          {/* Title below the image */}
-          <div className="bg-green-500 w-full py-4 rounded-b-2xl flex items-center justify-center">
+          {/* Title below the image without a separate background */}
+          <div className="w-full py-4 flex items-center justify-center">
             <h3 className="text-white text-xl font-bold">{title}</h3>
           </div>
         </div>
       </Link>
-      <span className='absolute top-2 right-2 bg-yellow-500 text-white p-2 rounded hover:bg-yellow-600 transition duration-200' onClick={handleBookmark}>
+      <span
+        className="absolute top-2 right-2 bg-yellow-500 text-white p-2 rounded hover:bg-yellow-600 transition duration-200"
+        onClick={handleBookmark}
+      >
         {bookmarked ? <i className="ri-bookmark-fill"></i> : <i className="ri-bookmark-line"></i>}
       </span>
     </div>
