70f5932da805bf5a7fc5247770faad87b7b9139b
meleo2125 <mukeshprajapat3093@gmail.com>
2024-09-21 21:50:39 +0530

    plant card

--- src/app/(pages)/Tour/page.jsx
+++ src/app/(pages)/Tour/page.jsx
@@ -8,36 +8,36 @@ function Tour() {
     {
       name: "Medicinal",
       cards: {
-        image: "/assets/img/giloy.png",
-        title: "Giloy",
-        description: "Known as the root of immortality, Giloy is widely used in Ayurvedic medicine to boost immunity, treat fever, and detoxify the body.",
+        image: "/assets/img/themes/medicinal.png",
+        title: "Medicinal Theme",
+        description: "Discover plants known for their medicinal properties that offer natural remedies and promote holistic health.",
         backgroundColor: "#D4B86C"
       }
     },
     {
       name: "Skincare",
       cards: {
-        image: "/assets/img/turmeric1.png",
-        title: "Turmeric",
-        description: "Rich in anti-inflammatory and antioxidant properties, Turmeric is a well-known natural remedy for enhancing skin health, reducing acne, and promoting a glowing complexion.",
+        image: "/assets/img/themes/skincare.png",
+        title: "Skincare Theme",
+        description: "Explore plants with natural skincare benefits, known for enhancing beauty and promoting healthy, radiant skin.",
         backgroundColor: "#FF6F61"
       }
     },
     {
       name: "Haircare",
       cards: {
-        image: "/assets/img/amla.png",
-        title: "Amla",
-        description: "Amla, or Indian Gooseberry, is a powerful ingredient for hair health, used to strengthen hair follicles, prevent dandruff, and promote hair growth.",
+        image: "/assets/img/themes/haircare.png",
+        title: "Haircare Theme",
+        description: "Unveil the secrets of plants that nourish and strengthen hair, providing natural solutions for hair care.",
         backgroundColor: "#b53128"
       }
     },
     {
       name: "Immunity Booster",
       cards: {
-        image: "/assets/img/moringa.png",
-        title: "Moringa",
-        description: "Known as a superfood, Moringa is rich in vitamins and antioxidants, making it an excellent herbal remedy for boosting the immune system and overall health.",
+        image: "/assets/img/themes/immunity_booster.png",
+        title: "Immunity Booster Theme",
+        description: "Delve into plants that are packed with nutrients and antioxidants, essential for boosting immunity and overall well-being.",
         backgroundColor: "#ccaa4e"
       }
     }
@@ -54,20 +54,19 @@ function Tour() {
       <div className='container w-full grid grid-cols-1 gap-x-8 gap-y-8 cursor-pointer md:grid-cols-2'>
         {themes.map((theme) => (
 
-          <Link key={theme.cards.title}href={`/Tour/${theme.name.toLowerCase().replace(/\s+/g, '')}`}>
-          <div key={theme.name} className='space-y-4 p-5 hover:shadow-2xl hover:-translate-y-2 duration-200 bg-green-950 rounded-xl'>
-            <div className="text-2xl text-center font-bold text-white" style={{color: theme.cards.backgroundColor}} >{theme.name}</div>
-            <div className="container flex justify-center">
-              <Themecard
-                key={theme.cards.title}
-                image={theme.cards.image}
-                title={theme.cards.title}
-                description={theme.cards.description}
-                backgroundColor={theme.cards.backgroundColor}
-              />
+          <Link key={theme.cards.title} href={`/Tour/${theme.name.toLowerCase().replace(/\s+/g, '')}`}>
+            <div key={theme.name} className='space-y-4 p-5 hover:shadow-2xl hover:-translate-y-2 duration-200 bg-green-950 rounded-xl'>
+              <div className="text-2xl text-center font-bold text-white" style={{color: theme.cards.backgroundColor}}>{theme.name}</div>
+              <div className="container flex justify-center">
+                <Themecard
+                  key={theme.cards.title}
+                  image={theme.cards.image}
+                  title={theme.cards.title}
+                  description={theme.cards.description}
+                  backgroundColor={theme.cards.backgroundColor}
+                />
+              </div>
             </div>
-          </div>
-
           </Link>
         ))}
       </div>
